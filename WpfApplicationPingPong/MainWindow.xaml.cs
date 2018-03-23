6fa9a6543cabb873e02eb65f03b928d533796c15
jnszmb <zimmermannjonas@dv-schulen.de>
2018-03-23 10:06:16 +0100

    laal

--- WpfApplicationPingPong/MainWindow.xaml.cs
+++ WpfApplicationPingPong/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace WpfApplicationPingPong
     /// </summary>
     public partial class MainWindow : Window
     {
+        double vx = 210.0;
+        double vy = 220.0;
+        int spieler1;
+        int spieler2;
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.01);
+            timer.Tick += ticker;
+            timer.IsEnabled = true;
+        }
+
+        private void ticker(object sender, EventArgs e)
+        {
+            Double x = Canvas.GetLeft(ball);
+            Double y = Canvas.GetTop(ball);
+            
+            
+            x += vx * timer.Interval.TotalSeconds;
+            y += vy * timer.Interval.TotalSeconds;
+
+            Canvas.SetLeft(ball, x);
+            Canvas.SetRight(ball, y);
+            if(x <=0.0)
+            {
+                spieler2++;
+                MessageBox.Show("" + spieler1 + ":" + "" + spieler2);
+                vx = -vx;
+            }
+            if(x >= myCanvas.ActualWidth - ball.Width)
+            {
+                spieler1++;
+                MessageBox.Show("" + spieler1 + ":" + "" + spieler2);
+                vx = -vx;
+            }
+            if(x <= 0.0+ leftPaddle.Width || x >= myCanvas.ActualWidth - ball.Width - leftPaddle.Width)
+            {
+                if(y < Canvas.GetTop(leftPaddle)+leftPaddle.Height && y >= Canvas.GetTop(leftPaddle))
+                {
+                    vx = -vx * 1;
+                   
+                }
+                else
+                {
+                   
+                }
+            }
+           
+            
+            if (y <= 0.0 || y >= myCanvas.ActualHeight - ball.Height)
+            {
+                vy = -vy;
+            }
+            Canvas.SetTop(ball, y);
+           
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetTop(leftPaddle, p.Y);
+            Canvas.SetTop(rightPaddle, p.Y);
+            
         }
     }
 }
